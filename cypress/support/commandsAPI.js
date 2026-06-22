0b9b291895313b38f5e5525c583bd603df1a5611
Rommelfoxx <vanderlan.alves.filho@gmail.com>
2026-06-22 20:53:26 -0300

    Better use than fixture

--- cypress/support/commandsAPI.js
+++ cypress/support/commandsAPI.js
@@ -1,56 +1,34 @@
 const BASE_URL = Cypress.env('apiUrl')
 
-//Deleta carrinho 
-Cypress.Commands.add('deletarCarrinhoAPI', () => {
-  cy.request({
-    method: 'DELETE',
-    url: `${BASE_URL}/carrinho/1`,
-  }).then((response) => {
-    expect(response.status).to.eq(200)
-  })
-})
-
-//Adiciona produito no carrinho 
-Cypress.Commands.add('adicionarProdutoCarrinhoAPI', (productId,quantity,userId) => {
-  cy.request({
-    method: 'POST',
-    url: `${BASE_URL}/carrinho`,
-    body:{productId,quantity,userId}
-  }).then((response) => {
-    // expect(response.status).to.eq(201)
-  })
-})
-
 
 Cypress.Commands.add('listarCarrinhoAPI', (userId) => {
   return cy.request({
-      method: 'GET',
-      url: `${BASE_URL}/carrinho/${userId}`,
-      failOnStatusCode: false
+    method: 'GET',
+    url: `${BASE_URL}/carrinho/${userId}`,
+    failOnStatusCode: false
   })
 })
-
+//Adiciona produito no carrinho 
 Cypress.Commands.add('adicionarAoCarrinhoAPI', (payload) => {
   return cy.request({
-      method: 'POST',
-      url: `${BASE_URL}/carrinho`,
-      body: payload,
-      failOnStatusCode: false
+    method: 'POST',
+    url: `${BASE_URL}/carrinho`,
+    body: payload,
+    failOnStatusCode: false
   })
 })
-
 Cypress.Commands.add('removerItemCarrinhoAPI', (userId, productId) => {
   return cy.request({
-      method: 'DELETE',
-      url: `${BASE_URL}/carrinho/${userId}/${productId}`,
-      failOnStatusCode: false
+    method: 'DELETE',
+    url: `${BASE_URL}/carrinho/${userId}/${productId}`,
+    failOnStatusCode: false
   })
 })
-
-Cypress.Commands.add('limparCarrinhoAPI', (userId) => {
+//Deleta carrinho 
+Cypress.Commands.add('limparCarrinhoAPI', (userId = 1) => {
   return cy.request({
-      method: 'DELETE',
-      url: `${BASE_URL}/carrinho/${userId}`,
-      failOnStatusCode: false
+    method: 'DELETE',
+    url: `${BASE_URL}/carrinho/${userId}`,
+    failOnStatusCode: false
   })
 })
